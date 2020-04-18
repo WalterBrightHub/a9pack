e9ff730b3bdffae3db395a71f318202052da1c08
Walter <ssmbuild@outlook.com>
2020-04-18 14:32:52 +0800

    commit

--- src/pages/jesko/conponents/PackSimulator/index.jsx
+++ src/pages/jesko/conponents/PackSimulator/index.jsx
@@ -1,33 +1,35 @@
 import React from "react";
 import {Card, Flex, Modal,} from "antd-mobile";
 import CopperPack from "../CopperPack";
-import CarCard from "../../../../conponents/CarCard";
+import CarCard from "../../../../components/CarCard";
 import carInfo from "../../../../model/carInfo";
 
 import '../../../../style/common.css'
-import RewordCard from "../../../../conponents/RewordCard";
+import RewordCard from "../../../../components/RewordModal/components/RewordCard";
+import RewordModal from "../../../../components/RewordModal";
 
 class PackSimulator extends React.Component {
   constructor(props) {
     super(props);
 
     const cars = [
-      'Senna',
+      'Jesko',
+      '488 GTB',
+      'Artega',
+      'G60',
+      'Centenario',
+      'P1',
       '003 S',
-      '2017 NSX',
-      'F-Type SVR',
+      'H2',
+      'M4 GTS',
       'Sprint 220',
       'DB11',
-      'AMG GT S',
-      '911 GTS Coupe',
-      '3.0 CSL',
-      'Challenger SRT8',
-      'Camaro LT',
-      'Lancer Evolution',
+      'DS E-Tense',
+      '50th'
     ]
 
     //除了这些车，其余默认满星
-    const notMaxCars = ['Senna']
+    const notMaxCars = ['Jesko', '488 GTB', 'Artega', 'G60']
 
     const garage = cars.reduce((garage, carName) => ({
       ...garage,
@@ -38,6 +40,7 @@ class PackSimulator extends React.Component {
 
 
     this.state = {
+      showReword: false,
       countCopperPack: 0,
       cars,
       count648: 0,
@@ -61,6 +64,7 @@ class PackSimulator extends React.Component {
 
     this.showModal = this.showModal.bind(this)
     this.onWrapTouchStart = this.onWrapTouchStart.bind(this)
+    this.onCloseRewordModal = this.onCloseRewordModal.bind(this)
 
 
   }
@@ -110,7 +114,10 @@ class PackSimulator extends React.Component {
   }
 
   onShowRewordModal(infos) {
-    this.setState({last_reword: infos})
+    this.setState({
+      last_reword: infos,
+      showRewordModal: true
+    })
     this.showModal('show_reword_modal')()
   }
 
@@ -138,89 +145,102 @@ class PackSimulator extends React.Component {
     });
   }
 
+  onCloseRewordModal() {
+    this.setState({
+      showRewordModal: false
+    })
+  }
+
   render() {
 
+    let {showRewordModal, last_reword} = this.state
 
-    return (
-      <div className='simulator'>
-        <Modal
-          visible={this.state.show_reword_modal}
-          transparent
-          onClose={this.onClose('show_reword_modal')}
-          title="恭喜！你获得了这些奖励!"
-          footer={[{
-            text: '继续', onPress: () => {
-              this.onClose('show_reword_modal')();
-            }
-          }]}
-          wrapProps={{onTouchStart: this.onWrapTouchStart}}
-        >
-          <div style={{height: `${1.2 + 2.1 * this.state.last_reword.length}em`, overflow: 'scroll'}}>
-            {this.state.last_reword.map((info, index) => (
-              <RewordCard key={index} info={info}></RewordCard>
-            ))}
+    return (<>
+        {showRewordModal
+          ? <RewordModal
+            onClose={this.onCloseRewordModal}
+            rewords={last_reword}
+          />
+          : <div className='simulator'>
+            <Modal
+              visible={false && this.state.show_reword_modal}
+              transparent
+              onClose={this.onClose('show_reword_modal')}
+              title="恭喜！你获得了这些奖励!"
+              footer={[{
+                text: '继续', onPress: () => {
+                  this.onClose('show_reword_modal')();
+                }
+              }]}
+              wrapProps={{onTouchStart: this.onWrapTouchStart}}
+            >
+              <div style={{height: `${1.2 + 2.1 * this.state.last_reword.length}em`, overflow: 'scroll'}}>
+                {this.state.last_reword.map((info, index) => (
+                  <RewordCard key={index} info={info}></RewordCard>
+                ))}
+              </div>
+            </Modal>
+
+            <div className='sub-title'>卡牌包</div>
+
+            <Card full className='content-body' style={{borderRadius: '10px', color: '#303030'}}>
+              <Card.Header
+                title="Koenigsegg Jesko"/>
+              <Card.Body>
+                <CopperPack
+                  countCopperPack={this.state.countCopperPack}
+                  pack_content={this.props.copperPackInfo}
+                  onAddCarCard={this.onAddCarCard}
+                  onAddCountCopperPack={this.onAddCountCopperPack}
+                  onAddCopper={this.onAddCopper}
+                  onAddCredit={this.onAddCredit}
+                  copper={this.state.copper}
+                  onAddCount648={this.onAddCount648}
+                  onShowRewordModal={this.onShowRewordModal}
+                />
+              </Card.Body>
+            </Card>
+
+
+            <div className='sub-title'>我的物品</div>
+            <Flex wrap='wrap' className='content-body'>
+              <Flex.Item className='coin-item'>
+                <Flex>
+                  <Flex.Item>点券</Flex.Item>
+                  <div className='coin-value'>{this.state.copper}</div>
+                </Flex>
+              </Flex.Item>
+              {/*<Flex.Item className='coin-item'>*/}
+              {/*  <Flex>*/}
+              {/*    <Flex.Item>金币</Flex.Item>*/}
+              {/*    <div className='coin-value'>{this.state.credit}</div>*/}
+              {/*  </Flex>*/}
+              {/*</Flex.Item>*/}
+
+            </Flex>
+
+            {/*<div className='sub-title'>充值记录</div>*/}
+            <div className='content-body total-kejin'>累计充值 <span
+              style={{color: '#ff0054', fontWeight: 'bold'}}>{this.state.count648}</span> 次 648 ，花费 <span
+              style={{color: '#ff0054', fontWeight: 'bold'}}>{this.state.count648 * 648}</span> 元
+            </div>
+
+            <div className='sub-title'>我的车库</div>
+            <div className='content-body garage'>
+              {this.state.cars.map(carCode => {
+                let car = carInfo[carCode]
+                return <CarCard
+                  firstName={car.firstName}
+                  lastName={car.lastName}
+                  starArray={car.starArray}
+                  cardNum={this.state.garage[carCode]}
+                  key={carCode}
+                />
+              })}
+            </div>
+
           </div>
-        </Modal>
-
-        <div className='sub-title'>卡牌包</div>
-
-        <Card full className='content-body' style={{borderRadius: '10px', color: '#303030'}}>
-          <Card.Header
-            title="McLaren Senna 限时卡包"/>
-          <Card.Body>
-            <CopperPack
-              countCopperPack={this.state.countCopperPack}
-              pack_content={this.props.copperPackInfo}
-              onAddCarCard={this.onAddCarCard}
-              onAddCountCopperPack={this.onAddCountCopperPack}
-              onAddCopper={this.onAddCopper}
-              onAddCredit={this.onAddCredit}
-              copper={this.state.copper}
-              onAddCount648={this.onAddCount648}
-              onShowRewordModal={this.onShowRewordModal}
-            />
-          </Card.Body>
-        </Card>
-
-
-        <div className='sub-title'>我的物品</div>
-        <Flex wrap='wrap' className='content-body'>
-          <Flex.Item className='coin-item'>
-            <Flex>
-              <Flex.Item>点券</Flex.Item>
-              <div className='coin-value'>{this.state.copper}</div>
-            </Flex>
-          </Flex.Item>
-          <Flex.Item className='coin-item'>
-            <Flex>
-              <Flex.Item>金币</Flex.Item>
-              <div className='coin-value'>{this.state.credit}</div>
-            </Flex>
-          </Flex.Item>
-
-        </Flex>
-
-        {/*<div className='sub-title'>充值记录</div>*/}
-        <div className='content-body total-kejin'>累计充值 <span
-          style={{color: '#ff0054', fontWeight: 'bold'}}>{this.state.count648}</span> 次 648 ，花费 <span
-          style={{color: '#ff0054', fontWeight: 'bold'}}>{this.state.count648 * 648}</span> 元
-        </div>
-
-        <div className='sub-title'>我的车库</div>
-        <div className='content-body garage'>
-          {this.state.cars.map(carCode => {
-            let car = carInfo[carCode]
-            return <CarCard
-              firstName={car.firstName}
-              lastName={car.lastName}
-              starArray={car.starArray}
-              cardNum={this.state.garage[car.lastName]}
-              key={carCode}
-            />
-          })}
-        </div>
-
-      </div>
+        }</>
     )
   }
 }
